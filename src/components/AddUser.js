54480b8fea3a2886802a550802afe7ed9bad2203
yumeyuniko <daigocafe@msn.com>
2022-04-01 18:17:36 +0900

    creatr the UI

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 export const AddUser = () => {
   return (
@@ -9,6 +10,9 @@ export const AddUser = () => {
         <Input type="text" placeholder="Enter Name"></Input>
       </FormGroup>
       <Button type="submit">Submit</Button>
+      <Link to="/" className="btn btn-danger ml-2">
+        Cancel
+      </Link>
     </Form>
   );
 };
--- src/components/EditUser.js
+++ src/components/EditUser.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 export const EditUser = () => {
   return (
-    <div>
-      <h1>EditUser</h1>
-    </div>
+    <Form>
+      <FormGroup>
+        <Label>Name</Label>
+        <Input type="text" placeholder="Enter Name"></Input>
+      </FormGroup>
+      <Button type="submit">Edit Name</Button>
+      <Link to="/" className="btn btn-danger ml-2">
+        Cancel
+      </Link>
+    </Form>
   );
 };
